21da1a3be98fd9343d5b41fda05074040339cde3
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-10-05 01:51:29 +0530

    Fixed formatting in the TextureManager files

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -7,73 +7,72 @@
 
 namespace MemoryGame
 {
-//static instance
-static TextureManager *sTextureManager = nullptr;
-
-TextureManager* TextureManager::Instance()
-{
-	if(sTextureManager == nullptr)
-		sTextureManager = new TextureManager();
-	return sTextureManager;
-}
-
-int TextureManager::load(const std::string& filepath, SDL_Renderer *renderer)
-{
-	SDL_Surface *surface = IMG_Load(filepath.c_str());
-	if (!surface) {
-		std::cerr << "ERROR: Could not load surface from file: " << filepath << '\n';
-		return -1;
-	}
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_FreeSurface(surface);
-	if (!texture) {
-		std::cerr << "Could not load texture from surface: " << SDL_GetError() << '\n';
-		return -1;
+	//static instance
+	static TextureManager *sTextureManager = nullptr;
+	TextureManager* TextureManager::Instance()
+	{
+		if(sTextureManager == nullptr)
+			sTextureManager = new TextureManager();
+		return sTextureManager;
 	}
 
-	mTextures[mCurrentId] = texture;
-	return mCurrentId++;
-}
+	int TextureManager::load(const std::string& filepath, SDL_Renderer *renderer)
+	{
+		SDL_Surface *surface = IMG_Load(filepath.c_str());
+		if (!surface) {
+			std::cerr << "ERROR: Could not load surface from file: " << filepath << '\n';
+			return -1;
+		}
+		SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_FreeSurface(surface);
+		if (!texture) {
+			std::cerr << "Could not load texture from surface: " << SDL_GetError() << '\n';
+			return -1;
+		}
 
-SDL_Texture* TextureManager::get(int id)
-{
-	if (auto it = mTextures.find(id); it != mTextures.end()) {
-		return it->second;
-	} else {
-		std::cerr << "Could not get texture with id: " << id << '\n';
-		return nullptr;
+		mTextures[mCurrentId] = texture;
+		return mCurrentId++;
 	}
-}
 
-void TextureManager::unload(int id)
-{
-	if (auto it = mTextures.find(id); it != mTextures.end()) {
-		SDL_DestroyTexture(it->second);
-	} else {
-		std::cerr << "Could not delete texture with id: " << id << '\n';
+	SDL_Texture* TextureManager::get(int id)
+	{
+		if (auto it = mTextures.find(id); it != mTextures.end()) {
+			return it->second;
+		} else {
+			std::cerr << "Could not get texture with id: " << id << '\n';
+			return nullptr;
+		}
 	}
-}
 
-TextureManager::TextureManager()
-	: mCurrentId(0)
-{
-	int flags = IMG_INIT_PNG;
-	int innited = IMG_Init(flags);
-	if((innited & flags) != flags) {
-		std::cerr << "Could not initialize SDL2_image: " << IMG_GetError() << '\n';
-		exit(1);
+	void TextureManager::unload(int id)
+	{
+		if (auto it = mTextures.find(id); it != mTextures.end()) {
+			SDL_DestroyTexture(it->second);
+		} else {
+			std::cerr << "Could not delete texture with id: " << id << '\n';
+		}
 	}
-}
 
-TextureManager::~TextureManager()
-{
-	IMG_Quit();
-	for (const auto &[_, texture] : mTextures)
-		SDL_DestroyTexture(texture);
-}
+	TextureManager::TextureManager()
+		: mCurrentId(0)
+	{
+		int flags = IMG_INIT_PNG;
+		int innited = IMG_Init(flags);
+		if((innited & flags) != flags) {
+			std::cerr << "Could not initialize SDL2_image: " << IMG_GetError() << '\n';
+			exit(1);
+		}
+	}
 
-bool TextureManager::exists(int id)
-{
-	return mTextures.find(id) != mTextures.end();
-}
+	TextureManager::~TextureManager()
+	{
+		IMG_Quit();
+		for (const auto &[_, texture] : mTextures)
+			SDL_DestroyTexture(texture);
+	}
+
+	bool TextureManager::exists(int id)
+	{
+		return mTextures.find(id) != mTextures.end();
+	}
 } // MemoryGame
--- src/TextureManager.h
+++ src/TextureManager.h
@@ -7,29 +7,29 @@
 
 namespace MemoryGame
 {
-class TextureManager
-{
-public:
-	/* get the singleton */
-	static TextureManager* Instance();
-	/* load a texture into the manager
-	 * returns a non-zero id if successful, -1 if unsuccessful
-	 */
-	int load(const std::string& filepath, SDL_Renderer *renderer);
-	/* get texture by the id
-	 * returns
-	 */
-	SDL_Texture* get(int id);
-	/* unload texture if it exists */
-	void unload(int id);
-	~TextureManager();
-private:
-	TextureManager();
-	TextureManager(const TextureManager&) = delete;
-	/* Check whether a texture with the given id exists exists */
-	bool exists(int id);
-private:
-	std::unordered_map<int, SDL_Texture*> mTextures;
-	int mCurrentId;
-};
+	class TextureManager
+	{
+	public:
+		/* get the singleton */
+		static TextureManager* Instance();
+		/* load a texture into the manager
+		 * returns a non-zero id if successful, -1 if unsuccessful
+		 */
+		int load(const std::string& filepath, SDL_Renderer *renderer);
+		/* get texture by the id
+		 * returns
+		 */
+		SDL_Texture* get(int id);
+		/* unload texture if it exists */
+		void unload(int id);
+		~TextureManager();
+	private:
+		TextureManager();
+		TextureManager(const TextureManager&) = delete;
+		/* Check whether a texture with the given id exists exists */
+		bool exists(int id);
+	private:
+		std::unordered_map<int, SDL_Texture*> mTextures;
+		int mCurrentId;
+	};
 }
